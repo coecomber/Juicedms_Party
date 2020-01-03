c2ab90e8e1ec74b32fa31ef5a689337f1301caf0
coecomber <joostvherwaarden@hotmail.com>
2020-01-03 15:04:25 +0100

    Initial commit

---
+++ src/test/java/com/Herwaarden/Party/PartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Herwaarden.Party;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
